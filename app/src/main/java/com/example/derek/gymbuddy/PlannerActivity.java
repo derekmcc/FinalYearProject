67c3b0a6a63755f906db5f94c8701c4a0a6c6d58
derekmcc <derek5mccarthy@gmail.com>
2018-11-13 18:08:01 +0000

    changed list view to a custom recycler view

--- app/src/main/java/com/example/derek/gymbuddy/PlannerActivity.java
+++ app/src/main/java/com/example/derek/gymbuddy/PlannerActivity.java
@@ -4,15 +4,23 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class PlannerActivity extends AppCompatActivity {
 
     private static final String TAG = "PlannerActivity";
     private FloatingActionButton addRoutineBtn;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +28,22 @@ public class PlannerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_planner);
 
         addRoutineBtn = findViewById(R.id.btnAddRoutine);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
 
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        String[] myDataset = new String[2];
+        myDataset[0] = "Derek";
+        myDataset[1] = "Shauna";
+        // specify an adapter (see also next example)
+        mAdapter = new PlannerAdapterActivity(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
     }//end onCreate method
 
     /**
---
+++ app/src/main/java/com/example/derek/gymbuddy/PlannerAdapterActivity.java
@@ -0,0 +1,66 @@
+package com.example.derek.gymbuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class PlannerAdapterActivity extends RecyclerView.Adapter<PlannerAdapterActivity.MyViewHolder>  {
+
+    private String[] mDataset;
+
+    public PlannerAdapterActivity(String[] myDataset) {
+        this.mDataset = myDataset;
+    }
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        // each data item is just a string in this case
+        public TextView routine, weight, numReps, numSets;
+        public MyViewHolder(View v) {
+            super(v);
+            routine = (TextView)v.findViewById(R.id.routine);
+            weight = (TextView)v.findViewById(R.id.weight);
+            numReps = (TextView)v.findViewById(R.id.numReps);
+            numSets = (TextView)v.findViewById(R.id.numSets);
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        View itemView =  LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.activity_planner_adapter, parent, false);
+
+        //MyViewHolder vh = new MyViewHolder(v);
+        return new MyViewHolder(itemView);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.routine.setText(mDataset[position]);
+        holder.weight.setText(mDataset[position]);
+        holder.numReps.setText(mDataset[position]);
+        holder.numSets.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
